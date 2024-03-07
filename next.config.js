234030132953e035c06051f2193a8864d792bacc
Vinhozin <pablovinicius4568@gmail.com>
2024-03-07 04:38:35 -0400

    dominios adicionados

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ['ifms.edu.br', 'ead.ifms.edu.br'],
+    },
+};
